b5a95bdb6f248bb3bd5517c9f8ab824dac3da390
Nghi Bien <nghibien@nghis-mbp-2.wireless.uic.edu>
2023-09-05 10:15:15 -0500

    Updates

---
+++ precache-manifest.e7d8dbfc26d0344bf091432df5aea017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ad11d5b01016d828e1ddd7cfc35468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e0187fe5995cd09933",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "c208d25aa0ae91f25f34",
+    "url": "/static/css/main.577335d7.chunk.css"
+  },
+  {
+    "revision": "44e0187fe5995cd09933",
+    "url": "/static/js/2.c5299e57.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c5299e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c208d25aa0ae91f25f34",
+    "url": "/static/js/main.9e41f25b.chunk.js"
+  },
+  {
+    "revision": "02f208948a1da3c72ff2",
+    "url": "/static/js/runtime-main.f7353c91.js"
+  }
+]);
